d75f26909e3d17db63b55b007cdd05feeb347e14
sijanpanta <cjan1102@gmail.com>
2026-01-05 14:42:23 +0545

    add get and post methods in http server in node js

--- day-2/server.js
+++ day-2/server.js
@@ -1,26 +1,49 @@
 const http = require("http");
-const { json } = require("stream/consumers");
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
+// Store the posted data here
+let storedData = null;
+
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-if(req.url==="/"){
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
     res.end("home page");
-}else if (req.url==="/about"){
-    res.end("About Page")}
-    else if (req.url==="/api/data"){
-        res.setHeader("Content-Type","application/json");
-        res.end(JSON.stringify({Name:"Sijan",Age:"19"}));
-    }
-    else{
-        res.statusCode=404;
-        res.end("Page not found");
-    }
-}
-);
+  } else if (req.url === "/about") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("About Page");
+  } else if (req.url === "/api/data" && req.method === "GET") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    // Return stored data or default data
+    const data = storedData || { name: "Sijan", age: 19 };
+    res.end(JSON.stringify(data));
+  } else if (req.url === "/api/data" && req.method === "POST") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("end", () => {
+      console.log("received:", body);
+      // Store the received data
+      try {
+        storedData = JSON.parse(body);
+      } catch (e) {
+        storedData = { raw: body };
+      }
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      res.end("data received");
+    });
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Page not found");
+  }
+});
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
