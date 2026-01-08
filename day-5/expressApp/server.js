7bb675f001e94010e51d34458df1948b1dd604a5
sijanpanta <cjan1102@gmail.com>
2026-01-08 12:07:11 +0545

    add query andparams in the api

--- day-5/expressApp/server.js
+++ day-5/expressApp/server.js
@@ -1,11 +1,28 @@
-const express= require("express");
+const express = require("express");
 const app = express();
+const port = process.env.PORT || 3030;
+let posts = [
+  { id: 1, title: "Post one" },
+  { id: 2, title: "Post two" },
+  { id: 3, title: "Post three" },
+];
 
-app.get("/",(req,res)=>{
-    res.status(500).send("check");
-     
-})
+//get all post
+app.get("/api/posts", (req, res) => {
+  const limit = parseInt(req.query.limit);
 
-app.listen(3000,()=>{
-    console.log("app is running in port: 3000")
+  if (!isNaN(limit) && limit > 0) {
+    res.json(posts.slice(0, limit));
+  } else {
+    res.json(posts);
+  }
+});
+
+app.get("/api/posts/:id", (req, res) => {
+  const id = req.params.id;
+  res.json(posts.filter((post) => post.id == id));
+});
+
+app.listen(port, () => {
+  console.log("app is running in port:" + port);
 });
