7e5742859d9582d4b61a948e51f660a1f1a3b4ee
sijanpanta <cjan1102@gmail.com>
2026-01-08 16:48:26 +0545

    Add CRUD operations and middleware - implemented POST, PUT, DELETE routes for posts API - added custom logger middleware - configured express.json() and urlencoded() middleware

--- day-5/expressApp/routes/posts.js
+++ day-5/expressApp/routes/posts.js
@@ -7,8 +7,9 @@ let posts = [
   { id: 3, title: "Post three" },
 ];
 
+
 //get all post
-router.get("/", (req, res) => {
+router.get("/",(req, res) => {
   const limit = parseInt(req.query.limit);
 
   if (!isNaN(limit) && limit > 0) {
@@ -28,4 +29,41 @@ router.get("/:id", (req, res) => {
   res.status(200).json(post);
 });
 
+//create new posts
+router.post("/",(req,res)=>{
+    const newPost={
+        id:posts.length+1,
+        title:req.body.title
+    };
+    if(!newPost.title){
+        res.status(400).json({message:"include a title"})
+    }
+    else{
+        posts.push(newPost)
+    }
+    res.status(201).json(posts);
+});
+
+//update post
+router.put("/:id",(req,res)=>{
+    const id=parseInt(req.params.id);
+    const post=posts.find(post=>post.id===id);
+   if(req.body.title){
+    post.title=req.body.title;
+   }else{
+    return res.status(400).json({error:"Enter title"})
+   }
+    res.status(200).json(posts);
+})
+
+//delete a post
+router.delete("/:id",(req,res)=>{
+    const id=parseInt(req.params.id);
+    const post=posts.find(post=>post.id===id);
+    if(!post){
+      return  res.status(404).json({message:"not found"});
+    }
+    posts=posts.filter(post=>post.id!=id);
+    res.json(posts);
+})
 export default router;
--- day-5/expressApp/server.js
+++ day-5/expressApp/server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import posts from "./routes/posts.js";
+import logger from "./middleware/logger.js";
 
 const app = express();
 const port = process.env.PORT || 3030;
 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+app.use(logger);
 app.use("/api/posts", posts);
 
 app.listen(port, () => {
   console.log("app is running in port:" + port);
 });
+ 
