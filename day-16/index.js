1e7e64ba2b35a91aa5c40dec7aa591f66f3af030
sijanpanta <cjan1102@gmail.com>
2026-01-25 12:36:40 +0545

    learned how to insert into the table and show the table

--- day-16/index.js
+++ day-16/index.js
@@ -7,13 +7,35 @@ const PORT = 3000;
 app.use(express.json());
 
 // MySQL connection
-const db = require("./models")
+const db = require("./models");
 
-//    
-db.sequelize.sync().then((req)=>{
+const { User } = require("./models");
 
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
-})
-// Start server
+app.get("/select", async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get("/insert", (req, res) => {
+  User.create({
+    firstName: "Sijan",
+    age: 21,
+  }).catch((err) => {
+    if (err) console.log(err);
+  });
+  res.send("inserted");
+});
+
+app.delete("/delete", (req, res) => {
+  res.send("delete");
+});
+//
+db.sequelize.sync().then((req) => {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+});
