16613304485e8a3bf549fa991335d9a0e557df7f
sijanpanta <cjan1102@gmail.com>
2026-01-28 16:36:20 +0545

    add basic level of msg sharing between sockets

--- day-19/chat-app/index.js
+++ day-19/chat-app/index.js
@@ -3,6 +3,18 @@ const http =require("http");
 const path =require("path")
 const app=express();
 const server=http.createServer(app);
+const {Server}=require("socket.io")
+
+
+const io=new Server(server);
+
+io.on("connection",socket=>{
+    console.log("A new user is connected",socket.id)
+    socket.on("message",message=>{
+        message=socket.id+'='+message;
+       socket.broadcast.emit('out-msg',message)
+    })
+})
 
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -10,3 +22,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 server.listen(3000,()=>{
     console.log("The server is running in port 3000")
 })
+
