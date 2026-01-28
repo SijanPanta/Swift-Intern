e5117c2f607cea9dc58362027c7e78135ebac633
sijanpanta <cjan1102@gmail.com>
2026-01-28 16:37:10 +0545

    made a seperate js file for frontend

--- day-19/chat-app/public/main.js
+++ day-19/chat-app/public/main.js
@@ -1,1 +1,18 @@
-// const socket
+ const socket = io();
+      const sendBtn = document.getElementById("sendBtn");
+      const messageInput = document.getElementById("message");
+      const inputMessage = messageInput.value;
+      sendBtn.addEventListener("click", (e) => clickFun(inputMessage));
+      messageInput.addEventListener("keydown", (e) => {
+        if (e.key === "Enter" && inputMessage) {
+          clickFun(inputMessage);
+        }
+      });
+
+      function clickFun(mes) {
+        socket.emit("message", mes);
+      }
+
+      socket.on("out-msg", (message) => {
+        console.log( message);
+      });
